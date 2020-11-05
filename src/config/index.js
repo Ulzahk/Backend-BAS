77d33245238489cfc054009c45b0d3497e0af860
Ulzahk <ulzahk@gmail.com>
2020-11-04 21:48:20 -0500

    [CREATED] config folder, changed  port and added cors

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+const config = {
+  port: process.env.PORT || 3500
+}
+
+module.exports = config
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
+const { port } = require('./config')
 
 // API
 const api = express()
 
 api.get('/', (req, res) => {
-  res.send(`Server Status: [🟢 Online]\nFor more information visit: https://github.com/Ulzahk/Backend-BAS`)
+  res.send('Server Status: [🟢 Online]\nFor more information visit: https://github.com/Ulzahk/Backend-BAS')
 })
 
-const server = api.listen(3000, (req, res) =>{
+const server = api.listen(port, () => {
   console.log(`Server listening at http://localhost:${server.address().port}`)
 })
